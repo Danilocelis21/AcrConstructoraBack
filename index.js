42387666e6b120cd9d33def17aca501ce9b8378f
Danilocelis21 <wilsoncelis_1998qhotmail.com>
2023-04-20 11:12:25 -0500

    primero commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express  = require ('express');
 const mysql = require ('mysql');
-
+const { count } = require('console');
 const app = express()
 
 app.use(express.json())
@@ -52,6 +52,73 @@ app.use((req, res, next) => {
     })
 
   })
+
+  app.get('/solicitud', (req,res) =>{
+    const sql = 'SELECT * FROM solicitud'
+
+    conectBD.query(sql, (error, result) =>{
+        if(error) throw error
+        if(result.length > 0){
+            res.json(result)
+        }else{
+            res.send('Not reults')
+        }
+    })
+})
+
+app.get('/solicitud/:id', (req,res) =>{
+
+    const id = req.params.id
+
+
+    const sql = `SELECT * FROM solicitud WHERE idsolicitud = ${id}`
+
+    conectBD.query(sql, (error, result) =>{
+        if (error) throw error
+
+        if(result.length > 0){
+            res.json(result)
+        }else{
+            res.send('Not results')
+        }
+    })
+})
+
+app.post('/agregar', (req,res) =>{
+
+    const sql = 'INSERT INTO solicitud SET ?'
+
+    const solicitudObj = {
+        idsolicitud: req.body.idsolicitud,
+        nombre: req.body.nombre,
+        correo: req.body.correo,
+        celular: req.body.celular,
+        tipoSolicitud: req.body.tipoSolicitud,
+        comentario: req.body.comentario
+    }
+
+    conectBD.query(sql, solicitudObj, error =>{
+        if(error) throw error
+
+        res.send('Solicitud agregada con exito.')
+    })
+
+})
+
+app.put('/actualizar/:id', (req,res) =>{
+
+    const idsolicitud= req.params.id
+    const{ nombre, correo, celular, tipoSolicitud, comentario } = req.body
+
+    const sql = `UPDATE solicitud SET nombre = '${nombre}', 
+    correo = '${correo}', celular= '${celular}', tipoSolicitud = '${tipoSolicitud}', 
+    comentario = '${comentario}' WHERE idsolicitud = ${idsolicitud}`
+
+    conectBD.query(sql, error =>{
+        if(error) throw error
+         res.send('Solicitud actualizada con exito')
+    })
+})
 
 
 app.listen(3001, console.log('Server running in port 3001'))
